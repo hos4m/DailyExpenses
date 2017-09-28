9d302d54e4cd7727c9c439d233517dd11a00220e
Hossam Mourad <hossam@alefeducation.com>
2017-09-28 20:17:12 +0400

    add App basic layout

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Container, Content, Tab, Tabs, TabHeading, Text } from 'native-base';
+import { FontAwesome } from "react-native-vector-icons";
+
+import AppHeader from "./components/header";
 
 export default class App extends React.Component {
+  generateHeading(name) {
+    return(
+      <TabHeading>
+        <FontAwesome name={name} style={styles.tabIcon} />
+      </TabHeading>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <AppHeader />
+        <Tabs tabBarPosition="bottom">
+          <Tab heading={this.generateHeading('money')}>
+            <Content style={styles.appContent}>
+              <Text>Tab 1</Text>
+            </Content>
+          </Tab>
+          <Tab heading={this.generateHeading('pie-chart')}>
+            <Content style={styles.appContent}>
+              <Text>Tab 2</Text>
+            </Content>
+          </Tab>
+          <Tab heading={this.generateHeading('list')}>
+            <Content style={styles.appContent}>
+              <Text>Tab 3</Text>
+            </Content>
+          </Tab>
+          <Tab heading={this.generateHeading('gears')}>
+            <Content style={styles.appContent}>
+              <Text>Tab 4</Text>
+            </Content>
+          </Tab>
+        </Tabs>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContent: {
+    padding: 10,
+  },
+
+  tabIcon: {
+    fontSize: 20,
   },
 });
---
+++ components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header, Body, Left, Right, Title } from "native-base";
+
+export default class AppHeader extends React.Component {
+  render() {
+    return (
+      <Header>
+        <Left />
+        <Body>
+          <Title>DailyExpenses</Title>
+        </Body>
+        <Right />
+      </Header>
+    )
+  }
+};

07e2df1355344baa39ed6cf73cf0d4f8050187eb
Hossam Mourad <ihos4m@gmail.com>
2017-10-18 21:35:41 +0400

    change the Header component to be stateless

--- components/header.js
+++ components/header.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Header, Body, Left, Right, Title } from "native-base";
 
-export default class AppHeader extends React.Component {
-  render() {
-    return (
-      <Header>
-        <Left />
-        <Body>
-          <Title>DailyExpenses</Title>
-        </Body>
-        <Right />
-      </Header>
-    )
-  }
-};
+export default AppHeader = () => (
+  <Header>
+    <Left />
+    <Body>
+      <Title>DailyExpenses</Title>
+    </Body>
+    <Right />
+  </Header>
+);

bb9825c12d4d12dd325efc57a5653031eb92bff8
Hossam Mourad <ihos4m@gmail.com>
2017-10-22 22:39:12 +0400

    add the expenses entries

--- screens/expenses/index.js
+++ screens/expenses/index.js
@@ -7,19 +7,34 @@ import styles from "./styles";
 
 export default class ExpensesScreen extends React.Component {
   onAddExpensesClick() {
-    alert('test');
+    alert("test");
   }
 
   render() {
     return (
       <View>
         <View style={styles.addButtonView}>
-          <Button onPress={this.onAddExpensesClick.bind(this)}>
+          <Button block onPress={this.onAddExpensesClick.bind(this)}>
             <FontAwesome name="plus" style={styles.addIcon} />
             <Text style={styles.addText}>Add Expenses</Text>
           </Button>
         </View>
+
+        {[1, 2, 3, 4].map(() => {
+          return [
+            <View style={styles.entryRow}>
+              <View>
+                <Text style={styles.entryCategory}>Taxi</Text>
+                <Text style={styles.entryDate}>12 Sep</Text>
+              </View>
+
+              <View>
+                <Text style={styles.entryExpense}>$15</Text>
+              </View>
+            </View>
+          ];
+        })}
       </View>
-    )
+    );
   }
-};
+}
--- screens/expenses/styles.js
+++ screens/expenses/styles.js
@@ -1,20 +1,48 @@
+import theme from "../../constants/theme";
+
 export default {
   addButtonView: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 15
   },
 
   addText: {
-    fontWeight: '900',
-    paddingLeft: 10,
+    fontWeight: "900",
+    paddingLeft: 10
   },
 
   addIcon: {
-    color: 'white',
+    color: "white",
     fontSize: 14,
-    fontWeight: '900',
-    marginLeft: 15,
+    fontWeight: "900",
+    marginLeft: 15
+  },
+
+  entryRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 10,
+    paddingHorizontal: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: theme.borderColor
+  },
+
+  entryCategory: {
+    fontSize: 18,
+    fontWeight: "700"
+  },
+
+  entryDate: {
+    fontSize: 14,
+    opacity: 0.6
+  },
+
+  entryExpense: {
+    fontSize: 22
   }
-}
+};

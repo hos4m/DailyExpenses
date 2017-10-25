30459d1195ca2f168ad91ecbbe3d44840c8bc401
Hossam Mourad <hossam@alefeducation.com>
2017-10-25 15:09:01 +0400

    add categories rows with edit and delete icons

--- screens/categories/index.js
+++ screens/categories/index.js
@@ -1,18 +1,51 @@
 import React from "react";
 import { View, Text } from "native-base";
+import { FontAwesome } from "react-native-vector-icons";
 
 import ActionButton from "../../components/actionButton";
 import styles from "./styles";
 
 export default class CategoriesScreen extends React.Component {
-  onAddCategoryPress() {
+  addCategory() {
     alert("Add Category");
+  }
+
+  editCategory() {
+    alert("Edit Cateogry");
+  }
+
+  deleteCategory() {
+    alert("Delete Category");
   }
 
   render() {
     return (
       <View>
-        <ActionButton title="Add Category" onPressFunc={() => this.onAddCategoryPress()} />
+        <ActionButton title="Add Category" onPressFunc={() => this.addCategory()} />
+
+        <View>
+          <View style={styles.entryRow}>
+            <View>
+              <Text style={styles.text}>Taxi</Text>
+            </View>
+
+            <View style={styles.iconsWrapper}>
+              <FontAwesome name="edit" style={styles.icon} onPress={() => this.editCategory()} />
+              <FontAwesome name="trash" style={[styles.icon, styles.deleteIcon]} onPress={() => this.deleteCategory()} />
+            </View>
+          </View>
+
+          <View style={styles.entryRow}>
+            <View>
+              <Text style={styles.text}>Food</Text>
+            </View>
+
+            <View style={styles.iconsWrapper}>
+              <FontAwesome name="edit" style={styles.icon} onPress={() => this.editCategory()} />
+              <FontAwesome name="trash" style={[styles.icon, styles.deleteIcon]} onPress={() => this.deleteCategory()} />
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
---
+++ screens/categories/styles.js
@@ -0,0 +1,33 @@
+import theme from "../../constants/theme";
+
+export default {
+  entryRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 10,
+    paddingHorizontal: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: theme.borderColor
+  },
+
+  text: {
+    fontSize: 18,
+    fontWeight: "700",
+  },
+
+  iconsWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  icon: {
+    fontSize: 30,
+    padding: 10,
+  },
+
+  deleteIcon: {
+    color: "red",
+  },
+}

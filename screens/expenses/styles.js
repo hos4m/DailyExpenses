3a1c84f62db9ee29b46dc395d6edcbcb2e7500f0
Hossam Mourad <ihos4m@gmail.com>
2017-10-30 00:11:39 +0400

    Add Expenses modal for adding new entries

--- screens/expenses/styles.js
+++ screens/expenses/styles.js
@@ -25,4 +25,36 @@ export default {
   entryExpense: {
     fontSize: 22
   },
+
+  modal: {
+    marginTop: 22,
+    padding: 22,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
+
+  modalForm: {
+    width: "100%",
+  },
+
+  modalFormItem: {
+    marginLeft: 0,
+  },
+
+  modalFormInput: {
+    paddingLeft: 15,
+  },
+
+  modalDollarSign: {
+    marginLeft: 15,
+  },
+
+  modalButtons: {
+    position: "absolute",
+    bottom: 22,
+    left: 22,
+    width: "100%"
+  },
 };
